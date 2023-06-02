9c0c1ad6f1765948a4aebbc3551ce72014ec2c25
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-06-02 17:39:51 +0300

    C init

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[100];
+    double deposits;
+    int duration;
+    double interest = 0.12;
+    double totalDeposits = 0;
+
+    printf("Enter a customer name: ");
+    scanf("%s", name);
+    printf("Enter initial deposit amount: ");
+    scanf("%1f", &deposits);
+    printf("Enter the duration in years: ");
+    scanf("%d", &duration);
+
+    for (int i = 0; i < duration; i++)
+    {
+        totalDeposits += deposits;
+        deposits += (deposits * interest);
+    }
+
+    printf("Total deposits for %s after %d is: %.2f\n", name, duration, totalDeposits);
+
+    return 0;
+}
